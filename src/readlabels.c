16702cdd97028a31fbc81f1c5c1e15c67aead203
Delyan Angelov <delian66@gmail.com>
2014-06-27 14:36:00 +0300

    Use the extracted idx module.

--- src/readlabels.c
+++ src/readlabels.c
@@ -2,35 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <arpa/inet.h>
-
-
-typedef struct {
-   unsigned char header[4];
-   int length;
-   unsigned char *data;
-} IDX_DATA_1 ;
-
-int read_idx_file_1( char *slabelfname, IDX_DATA_1 *idxdata){
-   FILE *f = fopen(slabelfname, "r+");
-   if(!f){
-      return 0;
-   }
-   
-   fread(&(idxdata->header),4,1,f);
-   if( idxdata->header[0] != 0 ) return 0;
-   if( idxdata->header[1] != 0 ) return 0;
-   if( idxdata->header[2] != 8 ) return 0; // the kind of data should be an unsigned byte
-   if( idxdata->header[3] != 1 ) return 0; // there should be only 1 dimension
-
-   fread(&(idxdata->length), 4,1,f);
-   idxdata->length = ntohl( idxdata->length );
-      
-   idxdata->data = calloc( idxdata->length, 1);
-   if( !idxdata->data ) return 0;
-   fread(idxdata->data, idxdata->length, 1, f);
-   return 1;
-}
-
+#include "idx.h"
 
 int main(int argc, char **argv){
    if(argc<2){
@@ -41,8 +13,8 @@ int main(int argc, char **argv){
    
    char *slabelfname = argv[1];
    
-   IDX_DATA_1 idxdata;
-   if(!read_idx_file_1( slabelfname, &idxdata)){
+   IDX1_DATA idxdata;
+   if(!fread_idx1_file( slabelfname, &idxdata)){
       printf("The datafile '%s' is not a valid IDX_1 file.\n", slabelfname);
       exit(2);
    }
