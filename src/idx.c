bfb94d2c3817a0fa6299d6760417a5acaca12fc2
Delyan Angelov <delian66@gmail.com>
2014-06-27 14:34:54 +0300

    Extract the read_idx function and struct type to a separate module idx .

---
+++ src/idx.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include "idx.h"
+
+int fread_idx1_file( char *slabelfname, IDX1_DATA *idxdata){
+   FILE *f = fopen(slabelfname, "r+");
+   if(!f){
+      return 0;
+   }
+   
+   fread(&(idxdata->header),4,1,f);
+   if( idxdata->header[0] != 0 ) return 0;
+   if( idxdata->header[1] != 0 ) return 0;
+   if( idxdata->header[2] != 8 ) return 0; // the kind of data should be an unsigned byte
+   if( idxdata->header[3] != 1 ) return 0; // there should be only 1 dimension
+
+   fread(&(idxdata->length), 4,1,f);
+   idxdata->length = ntohl( idxdata->length );
+      
+   idxdata->data = calloc( idxdata->length, 1);
+   if( !idxdata->data ) return 0;
+   fread(idxdata->data, idxdata->length, 1, f);
+   return 1;
+}
+
---
+++ src/idx.h
@@ -0,0 +1,20 @@
+#ifndef __IDX_H_ENTRY_H__
+#define __IDX_H_ENTRY_H__
+
+typedef struct {
+   unsigned char header[4];
+   int length;
+   unsigned char *data;
+} IDX1_DATA ;
+
+typedef struct {
+   unsigned char header[4];
+   int nimages;
+   int nrows;
+   int ncols;
+   unsigned char *data;
+} IDX3_DATA ;
+
+int fread_idx1_file( char *slabelfname, IDX1_DATA *idxdata);
+
+#endif  /* __IDX_H_ENTRY_H__ */
